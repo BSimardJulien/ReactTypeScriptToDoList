f5c933d4107d491859d23aa7a5d2729559918541
BSimardJulien <julien.b.simard@outlook.com>
2021-01-08 18:02:34 -0500

    items can be checked and uncheckd

--- todolistv2/src/App.tsx
+++ todolistv2/src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import {TodoListItem} from './TodoListItem'
 
-const  App: React.FC = ()=>{
-  return <div> yo</div>;
-};
+function App(){
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map(todo=>{
+      if(todo===selectedTodo){
+        return{
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  return(
+    <>
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo}/>
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo}/>
+    </>
+  );
+}
 
 export default App;
+
+const initialTodos : Todo[] = [
+  {
+    text: 'walk the dog',
+    complete: false
+  },
+  {
+    text: 'yeeet',
+    complete: true,
+  },
+];
---
+++ todolistv2/src/TodoListItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+interface Props {
+    todo : Todo;
+    toggleTodo : ToggleTodo;
+}
+
+export const TodoListItem: React.FC<Props> = ({todo,toggleTodo}) => {
+    return(
+        <ItemList>
+            <ItemLabel{...todo}>
+                <ItemInput type="checkbox" 
+                checked={todo.complete} 
+                onClick={()=>{
+                    toggleTodo(todo);
+                }}> 
+                </ItemInput>
+                {todo.text}
+            </ItemLabel>
+        </ItemList>
+    );
+}
+
+
+const ItemList = styled.li`
+    background-color:pink;
+    text-align: center;
+`;
+
+const ItemLabel = styled.label<Todo>`
+    text-decoration: ${(props)=>props.complete? 'line-through' : undefined};
+`
+
+const ItemInput = styled.input`
+    
+`
+
+
+
+
